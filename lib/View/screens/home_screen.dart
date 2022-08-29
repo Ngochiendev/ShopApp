dd0b41f3e8e8d1c1e2c09b7525424a3a64df6050
Võ Ngọc Hiện <hiendev.official@gmail.com>
2022-08-29 15:15:09 +0700

    UI Home

--- lib/View/screens/home_screen.dart
+++ lib/View/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:shopapp/View/widgets/home/search_text_form.dart';
+import 'package:shopapp/View/widgets/text_ultils.dart';
+
+import '../widgets/home/card_item.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,6 +14,59 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Home'),),);
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 170,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 107, 220, 111),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  TextUltils(
+                    text: 'Find Your',
+                    color: Colors.white,
+                    fontsize: 25,
+                    fontWeight: FontWeight.w300,
+                    letterSpacing: 0.8,
+                  ),
+                  TextUltils(
+                    text: 'INSPIRATION',
+                    color: Colors.white,
+                    fontsize: 28,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 0.8,
+                  ),
+                  SizedBox(height: 10),
+                  SearchFormText(),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: TextUltils(
+              text: 'Category',
+              fontsize: 20,
+              fontWeight: FontWeight.w500,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(height: 30),
+          const CardItems(),
+        ],
+      ),
+    );
   }
 }
